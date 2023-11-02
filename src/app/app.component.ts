d789038c8b05a27e0cb39b1a9457f7ff94633c7d
DESKTOP-CK0T8IJ\minhq <minhquyen314@gmail.com>
2023-11-02 12:06:20 +0900

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Carousel, Dropdown, initTE } from 'tw-elements';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  ngOnInit() {
+    initTE({ Carousel, Dropdown });
+  }
 }
